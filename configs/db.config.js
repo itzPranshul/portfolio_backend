8463d1a61579f55b53a72adbe36d35befb156bf3
pranshul gupta <pranshulg01@gmail.com>
2024-09-26 00:54:34 +0530

    adding mongodb atlas as DB

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    URI : "mongodb://localhost/portfolio_be",
+    URI : "mongodb+srv://pranshulg01:pranshul123@users.vbwc2.mongodb.net/?retryWrites=true&w=majority&appName=users",
     db_name :"portfolio_be"
 }
